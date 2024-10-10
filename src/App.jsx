86dc5edc4e407500097b8d4da5379d897b2d9e02
Udit verma <uditv04@gmail.com>
2024-10-10 14:07:21 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Navbar/>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
